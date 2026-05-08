71bf968b280a19d77bce2c25ac0b5f361419b6f6
The2028 <lomnsisi@gmail.com>
2026-05-08 19:35:29 +0000

    Initial commit - EduTrack backend

--- BackEnd/database/connexion.py
+++ BackEnd/database/connexion.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/EduTrack_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:marouani@localhost:5432/EduTrack"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
